c3ef6edc78e4724ad49ba660cf3990fcfa228810
xiaojianli <376127890@qq.com>
2018-03-16 15:46:27 +0800

    调整样式

--- common/common.js
+++ common/common.js
@@ -5,14 +5,10 @@ function createStyle() {
     var width = document.body.clientWidth;
     var height = document.body.clientHeight;
     var nod = document.createElement('style'),
-        str = '.step{width:'+parseInt(width*0.98)+'px;height:'+parseInt(height*0.96)+'px;}';
+        str = '.step{width:'+parseInt(width*0.98)+'px;height:'+parseInt(height*0.98)+'px;}';
     console.log(width);
     nod.type='text/css';
-    if(nod.styleSheet){         //ie下
-        nod.styleSheet.cssText = str;
-    } else {
-        nod.innerHTML = str;       //或者写成 nod.appendChild(document.createTextNode(str))
-    }
+    nod.innerHTML = str;
     document.getElementsByTagName('head')[0].appendChild(nod);
 }
 createStyle();
